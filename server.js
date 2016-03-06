3a7c0c8427c6e48e9d86cce853c64b547e8877e5
bogdanluncasu <tna_bogdan54@yahoo.com>
2016-03-06 23:50:00 +0200

    Added files via upload

---
+++ server.js
@@ -0,0 +1,39 @@
+var express=require("express"),
+app=express(),
+server=require("http").createServer(app),
+io=require("socket.io").listen(server);
+nicknames=[];
+server.listen(8888);
+
+app.get("/",function(require,response){
+	response.sendFile(__dirname + "/index.html");
+});
+
+app.use(express.static(__dirname + '/'));
+
+io.sockets.on('connection',function(socket){
+	
+	socket.on('send_msg',function(data){
+	io.sockets.emit('new_msg',socket.name+" : "+data);
+	});
+	socket.on('new_user',function(data,callback){
+		if(nicknames.indexOf(data)!=-1)callback(false);
+		else{
+			callback(true);
+			socket.name=data;
+			nicknames.push(socket.name);
+			updateNames();
+		}
+	});
+	function updateNames(){
+		io.sockets.emit('usernames',nicknames);
+	}
+	socket.on('disconnect',function(data){
+		var index;
+		index = nicknames.indexOf(socket.name);
+		nicknames.splice(index, 1);
+		updateNames();
+	});
+	
+});	
+	
